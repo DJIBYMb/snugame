012d82f541b7564604fa1ddb03be7f46f14e329e
DJIBYMb <mboupdjiby967@gmail.com>
2026-06-26 19:53:43 +0200

    Ajout favicon

--- server.js
+++ server.js
@@ -26,11 +26,6 @@ const rateLimit = require("express-rate-limit");
 const nodemailer = require("nodemailer");
 const compression = require("compression");
 
-const mesRecompensesBox =
-  document.getElementById("mesRecompensesBox");
-
-const mesRecompensesList =
-  document.getElementById("mesRecompensesList");
 
 if(process.env.FIREBASE_SERVICE_ACCOUNT){
 
@@ -39,7 +34,7 @@ if(process.env.FIREBASE_SERVICE_ACCOUNT){
 
   if(!getApps().length){
     initializeApp({
-      credential: cert(serviceAccount)
+      creential: cert(serviceAccount)
     });
   }
 
